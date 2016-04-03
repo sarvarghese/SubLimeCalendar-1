c5594e4a5c6618f03443faabdf4e33cdacf6905f
Eric <ericb713@gmail.com>
2016-04-03 17:45:13 -0500

    Revert "Eric I made some changes Nothing major "

--- app/src/main/java/com/example/oose/sublimecalendar/MainActivity.java
+++ app/src/main/java/com/example/oose/sublimecalendar/MainActivity.java
@@ -126,13 +126,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.month_view) {
+        if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.week_view) {
+        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.day_view) {
+        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.event_view) {
+        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
