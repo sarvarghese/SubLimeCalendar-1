ef86c7c27b9c9af7acd8d3964e8a3a4098e35284
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2016-04-11 14:04:08 -0500

    - Fixed the Event List View. It now updates data when ever it comes back from adding an event or viewing an event - Clicking on the events now takes you to the single event view of that event

--- app/src/main/java/com/example/oose/sublimecalendar/EventRecyclerAdapter.java
+++ app/src/main/java/com/example/oose/sublimecalendar/EventRecyclerAdapter.java
@@ -1,20 +1,12 @@
 package com.example.oose.sublimecalendar;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import java.sql.Date;
-import java.sql.Time;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
-
 /**
  * <code>RecyclerView.Adapter</code> is used by the <code>RecyclerView</code> to convert dynamic data
  * into viewable content on the screen. <code>RecyclerView</code> is designed to avoid making
@@ -23,15 +15,20 @@ import java.util.StringTokenizer;
  *
  * - Joseph Tompkins
  */
-public class EventRecyclerAdapter extends RecyclerView.Adapter<EventRecyclerAdapter.EventRecyclerViewHolder> {
+public class EventRecyclerAdapter extends RecyclerView.Adapter<EventRecyclerViewHolder> {
 
     private LayoutInflater inflater;
     private ArrayList<Event> eventList = new ArrayList<>();
+    private View.OnClickListener mOnClickListener;
+    private int eventRowViewID = R.id.event_row;
 
-    public EventRecyclerAdapter(Context context) {
+    public EventRecyclerAdapter(Context context, View.OnClickListener clickListener) {
         inflater = LayoutInflater.from(context);
 
-        eventList = (ArrayList<Event>) Event.listAll(Event.class);
+        mOnClickListener = clickListener;
+
+//        eventList = (ArrayList<Event>) Event.listAll(Event.class, "date asc");
+        updateData();
     }
 
     /**
@@ -39,11 +36,12 @@ public class EventRecyclerAdapter extends RecyclerView.Adapter<EventRecyclerAdap
      * containing the <code>RecyclerView</code> using this adapter to pass <code>Event</code>
      * objects via a <code>List</code>. Those <code>Event</code> objects are then used to inflate
      * the individual <code>View</code> objects in the <code>RecyclerView</code>.
-     * @param context the <code>Context</code> that this <code>Adapter</code> belongs to
+     *
+     * @param context   the <code>Context</code> that this <code>Adapter</code> belongs to
      * @param eventList the list of events to be displayed
      */
-    public EventRecyclerAdapter(Context context, ArrayList<Event> eventList) {
-        this(context);
+    public EventRecyclerAdapter(Context context, View.OnClickListener clickListener, ArrayList<Event> eventList) {
+        this(context, clickListener);
 
         this.eventList = eventList;
     }
@@ -54,6 +52,8 @@ public class EventRecyclerAdapter extends RecyclerView.Adapter<EventRecyclerAdap
 
         EventRecyclerViewHolder holder = new EventRecyclerViewHolder(view);
 
+        view.setOnClickListener(mOnClickListener);
+
         return holder;
     }
 
@@ -61,38 +61,8 @@ public class EventRecyclerAdapter extends RecyclerView.Adapter<EventRecyclerAdap
     public void onBindViewHolder(EventRecyclerViewHolder holder, int position) {
         Event current = eventList.get(position);
 
-        if(current != null) {
-            holder.eventName.setText(current.name);
-
-            //holder.eventDate.setText(dateToString(current.date));
-            //http://stackoverflow.com/questions/11755534/how-to-convert-long-date-value-to-mm-dd-yyyy-format
-            Date d=new Date(current.date);
-            SimpleDateFormat df2=new SimpleDateFormat("MM/dd/yyyy");
-            holder.eventDate.setText(df2.format(d));
-            /*holder.eventDuration.setText(timeToString(current.startTime) +
-                    " - " + timeToString(current.finishTime));*/
-            Date starttimestamp= new Date(current.startTime);
-            Date finishtimestamp= new Date(current.finishTime);
-            holder.eventDuration.setText(starttimestamp.getHours()+":"+starttimestamp.getMinutes()
-                    + " - " + finishtimestamp.getHours()+":"+finishtimestamp.getMinutes());
-
-            if(current.eventType != null) {
-                switch (current.eventType) {
-                    case "business":
-                        holder.eventColorDot.getDrawable().setTint(Color.argb(255, 180, 100, 100));
-                        break;
-                    case "birthday":
-                        holder.eventColorDot.getDrawable().setTint(Color.argb(255, 180, 180, 100));
-                        break;
-                    case "personal":
-                        holder.eventColorDot.getDrawable().setTint(Color.argb(255, 100, 180, 100));
-                        break;
-                    default:
-                        holder.eventColorDot.getDrawable().setTint(Color.argb(255, 100, 180, 100));
-                        break;
-                }
-            }
-            else holder.eventColorDot.getDrawable().setTint(Color.argb(255, 100, 180, 100));
+        if (current != null) {
+            holder.setEvent(current);
         }
     }
 
@@ -101,66 +71,9 @@ public class EventRecyclerAdapter extends RecyclerView.Adapter<EventRecyclerAdap
         return eventList.size();
     }
 
-    class EventRecyclerViewHolder extends RecyclerView.ViewHolder {
-        protected TextView eventName;
-        protected TextView eventDate;
-        protected TextView eventDuration;
-        protected ImageView eventColorDot;
+    public void updateData() {
+        eventList = (ArrayList<Event>) Event.listAll(Event.class, "date asc, start_time asc, finish_time asc");
 
-        public EventRecyclerViewHolder(View itemView) {
-            super(itemView);
-
-            eventName = (TextView) itemView.findViewById(R.id.event_name);
-            eventDate = (TextView) itemView.findViewById(R.id.event_date);
-            eventDuration = (TextView) itemView.findViewById(R.id.event_duration);
-            eventColorDot = (ImageView) itemView.findViewById(R.id.event_color_dot);
-        }
-    }
-
-    /**
-     * Converts <code>java.sql.Date()</code> to the <code>String</code> with format
-     * <code><i>mm/dd/yyyy</i></code>.
-     * @param date
-     * @return <code>String</code>
-     * of the format <code><i>mm/dd/yyyy</i></code>. If <code>date</code> parameter is
-     * <code>null</code>, an empty string is returned.
-     */
-    private String dateToString(Date date) {
-        if(date != null) {
-            StringTokenizer tok = new StringTokenizer(date.toString(), "-");
-            String year = tok.nextToken();
-            int month = Integer.parseInt(tok.nextToken()); // strips month number of leading zeros
-            int day = Integer.parseInt(tok.nextToken()); // strips day number of leading zeros
-
-            return month + "/" + day + "/" + year;
-        }
-
-        return "";
-    }
-
-    /**
-     * Converts <code>java.sql.Date()</code> to the <code>String</code> with format
-     * <code><i>hh:mm[am|pm]</i></code>.
-     * @param time
-     * @return <code>String</code> of format <code>hh:mm[am|pm]</code>. If <code>time</code>
-     * is <code>null</code>, an empty string is returned.
-     */
-    private String timeToString(Time time) {
-        if(time != null) {
-            StringTokenizer tok = new StringTokenizer(time.toString(), ":");
-
-            int hours = Integer.parseInt(tok.nextToken());
-            String minutes = tok.nextToken();
-
-            if (hours > 11 && hours < 24) {
-                if(hours != 12) hours -= 12;
-                return hours + ":" + minutes + "pm";
-            }
-
-            if(hours == 0) hours = 12;
-            return hours + ":" + minutes + "am";
-        }
-
-        return "";
+        notifyDataSetChanged();
     }
 }
